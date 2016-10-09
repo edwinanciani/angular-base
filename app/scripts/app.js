f1dee4d06eb5aaf966aa3261d9713a2c3ab6e772
Edwin Anciani <edwinanciani@gmail.com>
2016-10-08 22:13:57 -0600

    First commit: Arquitecture

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ckappApp
+ * @description
+ * # ckappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ckappApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ckappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ckappApp
+ */
+angular.module('ckappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
